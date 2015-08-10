3a73b5fefd62f1275e03e649c5f3d24d4e9a7c9c
Julianna Green & Ryan Heupler <student@epicodus.com>
2015-08-10 12:04:14 -0700

    Adds second spec for calculating weight times volume of parcel. Fails because of unwritten method

--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -8,4 +8,10 @@ describe(Parcel) do
     expect(new_parcel.volume()).to(eq(8))
     end
   end
+  describe('#cost_to_ship') do
+    it('calculates the weight times the volume') do
+    new_parcel = Parcel.new(2, 2, 2, 2,)
+    expect(new_parcel.cost_to_ship()).to(eq(16))
+    end
+  end
 end
