ee2359c83cb54e1dea9fd5e3e9170674a0845892
Julianna Green & Ryan Heupler <student@epicodus.com>
2015-08-10 12:29:09 -0700

    Second spec passes with cost_to_ship method applied which calculates the price of shipping.

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,13 +1,19 @@
 class Parcel
-  def initialize(length, width, height, weight)
+  def initialize(length, width, height, weight, distance)
     @length = length
     @width = width
     @height = height
     @weight = weight
+    @distance = distance
   end
 
   def volume
-    @length.*(@width.*(@height))
+    @length * @width * @height
+  end
 
+  def cost_to_ship
+    total_package = volume * @weight
+    total = @distance.*(0.01)
+    total = total + total_package
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -4,14 +4,14 @@ require('parcel')
 describe(Parcel) do
   describe('#volume') do
     it('calculates the volume of a parcel') do
-    new_parcel = Parcel.new(2, 2, 2, 2)
+    new_parcel = Parcel.new(2, 2, 2, 2, 20)
     expect(new_parcel.volume()).to(eq(8))
     end
   end
   describe('#cost_to_ship') do
-    it('calculates the weight times the volume') do
-    new_parcel = Parcel.new(2, 2, 2, 2,)
-    expect(new_parcel.cost_to_ship()).to(eq(16))
+    it('calculates the weight and distance of parcel and returns cost') do
+    new_parcel = Parcel.new(2, 2, 2, 2, 20)
+    expect(new_parcel.cost_to_ship()).to(eq(16.20))
     end
   end
 end
